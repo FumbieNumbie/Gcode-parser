c2f87aba8db956fc6c446f7475447d33138277b6
FumbieNumbie <a.alexeev@hotmail.com>
2018-12-16 12:20:19 +0300

    Included retracts in calculations. Huge boost to accuracy.

--- TextParser/Program.cs
+++ TextParser/Program.cs
@@ -21,9 +21,9 @@ namespace TextParser
 				}
 				if (Array.IndexOf(args, "-help") != -1)
 				{
-					Console.WriteLine("-abs -- the program will run in absolute values mode.");
+					Console.WriteLine("  -abs -- the program will run in absolute values mode.");
 					Console.WriteLine();
-					Console.WriteLine("Without parameters the program will compare the score to the baseline which is Ender3 test dog (0.4 mm extruder and 1.75 mm filament).");
+					Console.WriteLine("  Without parameters the program will compare the score to the baseline which is Ender3 test dog (0.4 mm extruder and 1.75 mm filament).");
 					Environment.Exit(0);
 				}
 			}
@@ -50,12 +50,15 @@ namespace TextParser
 
 			string[] lines = fileContents.Split('\n');
 			List<string> refinedList = RefineList(lines);
-
+			//foreach (string item in refinedList)
+			//{
+			//	Console.WriteLine(item);
+			//}
 
 			ratio = GetRatio(refinedList, abs);
 
-			Console.WriteLine("Ratio: " + ratio);
-			Console.WriteLine("Elapsed time: " + (DateTime.Now - now).TotalSeconds);
+			Console.WriteLine("Ratio: " + Math.Round(ratio,2));
+			Console.WriteLine("Elapsed time: " + Math.Round((DateTime.Now - now).TotalSeconds,1)+ " sec");
 			Console.WriteLine("Would you like to parse another file?");
 			Console.WriteLine("Y/N (default is 'Y')");
 			string answer = Console.ReadLine();
@@ -65,7 +68,6 @@ namespace TextParser
 			}
 			else
 			{
-				Console.Write("Enter a file name: ");
 				goto Mark;
 			}
 		}
@@ -98,7 +100,7 @@ namespace TextParser
 			{
 				Regex pattern1 = new Regex(@"X\d+\.*\d*");
 				Regex pattern2 = new Regex(@"\sY\d+\.*\d*");
-				Regex pattern3 = new Regex(@"\sE\d+\.*\d*");
+				Regex pattern3 = new Regex(@"E\d+\.*\d*");
 				Match match1 = pattern1.Match(item);
 				Match match2 = pattern2.Match(item);
 				Match match3 = pattern3.Match(item);
@@ -115,12 +117,13 @@ namespace TextParser
 				}
 				if (match3.Success)
 				{
-					line += match3.Value;
+					line += " " + match3.Value;
 				}
 				if (match1.Success)
 				{
 					list.Add(line);
 				}
+
 			}
 			return list;
 		}
@@ -131,8 +134,8 @@ namespace TextParser
 		private static double GetRatio(List<string> refList, bool absolute = false)
 		{
 			double totalLength = 0;
-			float lastE = 0, currentE = 0;
-			float deltaE = 0;
+			float currentE = 0;
+			float lastE = 0;
 			float totalE = 0;
 			for (int i = 1; i < refList.Count; i++)
 			{
@@ -141,22 +144,32 @@ namespace TextParser
 				if (match.Success)
 				{
 					currentE = float.Parse(match.Groups[1].Value.Substring(1));
-					deltaE = currentE - lastE;
-					if (deltaE != currentE)
+
+					if (lastE > currentE)
 					{
-						totalE += deltaE;
+						totalE += lastE;
 					}
 					lastE = currentE;
 				}
+				if (i == refList.Count-1)
+				{
+					Regex anyE = new Regex(@"(E\d+\.*\d*)");
+					Match anyMatch = anyE.Match(refList[i]);
+					if (anyMatch.Success)
+					{
+						totalE += float.Parse(anyMatch.Value.Substring(1));
+					}
+				}
+
 				bool hop = CheckHops(refList[i - 1], refList[i]);
 				if (!hop) //if these two lines are not a hop
 				{
-
 					Vector start = GetSegmentPoint(refList, i - 1);
 					Vector end = GetSegmentPoint(refList, i);
 					Vector segment = end - start;
 					totalLength += segment.Length();
 				}
+
 			}
 			if (absolute == true)
 			{
@@ -164,7 +177,7 @@ namespace TextParser
 			}
 			else
 			{
-				return 172.2558 / totalLength * totalE;
+				return 40.52 / totalLength * totalE;
 			}
 		}
 		/// <summary>
@@ -189,14 +202,17 @@ namespace TextParser
 		/// <returns></returns>
 		private static bool CheckHops(string st1, string st2)
 		{
-			Regex pattern = new Regex(@"E\d+\.*\d*");
-			Match match1 = pattern.Match(st1);
-			Match match2 = pattern.Match(st2);
-			if (!match1.Success && match2.Success)
+			Regex pattern1 = new Regex(@"X\d+\.*\d*");
+			Regex pattern2 = new Regex(@"\sY\d+\.*\d*");
+			Regex pattern3 = new Regex(@"\sE\d+\.*\d*");
+			bool x = pattern1.Match(st2).Success;
+			bool y = pattern2.Match(st2).Success;
+			bool e = pattern3.Match(st2).Success;
+			if (!pattern1.Match(st1).Success)
 			{
-				return false;
+				return true;
 			}
-			else if (match1.Success && match2.Success)
+			if (x && y && e)
 			{
 				return false;
 			}
